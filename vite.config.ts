967598570380d79b7d4004f8478907fdc831f480
Yura Motrych <yulemd@gmail.com>
2025-11-08 11:07:49 +0200

    add github action for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/star-wars-heroes/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
